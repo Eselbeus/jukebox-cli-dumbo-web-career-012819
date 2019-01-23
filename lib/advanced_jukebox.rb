a4eac3e160662efc35221198877f820d57654230
eselbeus <eselbeus@gmail.com>
2019-01-23 07:13:18 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -42,17 +42,16 @@ def play(my_songs)
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
   valid = false
-  system 'open /home/brave-programmer-8559/jukebox-cli-dumbo-web-career-012819/jukebox-cli/audio/Emerald-Park/03.mp3'
   puts "Please enter a song name or number\n"
   user_song = gets.chomp
-  # my_songs.each do |song, data|
-  #   split_data = data.split(">")
-  #   path = split_data[1]
-  #   if user_song == song
-  #     system 'open /home/brave-programmer-8559/jukebox-cli-dumbo-web-career-012819/jukebox-cli/audio/Emerald-Park/01.mp3'
-  #     valid = true
-  #   end
-  # end
+  my_songs.each do |song, data|
+    split_data = data.split(">")
+    path = split_data[1]
+    if user_song == song
+      system 'open /home/brave-programmer-8559/jukebox-cli-dumbo-web-career-012819/jukebox-cli/audio/Emerald-Park/01.mp3'
+      valid = true
+    end
+  end
   if valid == false
     puts "Invalid input, please try again\n"
   end
